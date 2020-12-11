426b9ca8f1912519b07e76b4a00cf0c97915f767
phuong <daut.phuong@gmail.com>
2020-12-11 09:10:12 +0700

    update login

---
+++ src/main/java/com/dtubot/controller/UserController.java
@@ -0,0 +1,66 @@
+package com.dtubot.controller;
+
+import com.dtubot.entity.security.login_msg.request.Login;
+import com.dtubot.entity.security.login_msg.response.JwtResponse;
+import com.dtubot.entity.security.utils.UserPrincipal;
+import com.dtubot.service.security.JwtProvider;
+import com.dtubot.service.security.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    JwtProvider jwtProvider;
+    @Autowired
+    AuthenticationManager authManager;
+
+    private PasswordEncoder encoder;
+
+    @Autowired
+    public void setEncoder(PasswordEncoder encoder) {
+        this.encoder = encoder;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody Login loginRequest) throws AuthenticationException {
+
+        Authentication authentication = authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtProvider.generatingJwt(authentication);
+        System.out.println("Token is generated: " + token);
+
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        System.out.println("UserDetails: " + userPrincipal.getUsername());
+
+        JwtResponse response = new JwtResponse(
+                userPrincipal.getId().toString(),
+                token,
+                userPrincipal.getUsername(),
+                userPrincipal.getEmail(),
+                userPrincipal.getAuthorities()
+        );
+        System.out.println(response);
+        return ResponseEntity.ok(response);
+    }
+}
